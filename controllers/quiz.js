94a0b1f2384fbb016c2fcc5c86af543c7f05fbad
Susana Fernández <susana.fernandez.redondo@alumnos.upm.es>
2018-06-02 10:20:17 +0200

    intento2

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -8,9 +8,17 @@ const paginate = require('../helpers/paginate').paginate;
 exports.load = (req, res, next, quizId) => {
 
     models.quiz.findById(quizId, {
-        include: [
-            models.tip,
-            {model: models.user, as: 'author'}
+         include: [
+            {
+                model: models.tip,
+                include: [
+                    {model: models.user, as: 'author'}
+                ]
+            },
+            {
+                model: models.user,
+                as: 'author'
+            }
         ]
     })
     .then(quiz => {
